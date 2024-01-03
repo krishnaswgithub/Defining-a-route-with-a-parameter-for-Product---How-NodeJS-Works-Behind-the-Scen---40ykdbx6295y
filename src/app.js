961faa8d54b4a8a84c18b0e7910ea4ca8a0e664a
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2024-01-03 22:20:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+   const productId=req.params.productId;
+  res.status(200).json({productId});
 });
 
 module.exports = app;
